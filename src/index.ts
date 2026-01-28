dd5398c9a017643ec93712cff09982989ce7ffc6
WKBASFernando <sankalpaangelo@gmail.com>
2026-01-28 21:59:54 +0530

    port in index

--- src/index.ts
+++ src/index.ts
@@ -69,7 +69,7 @@ app.get("/", (req, res) => {
   );
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
